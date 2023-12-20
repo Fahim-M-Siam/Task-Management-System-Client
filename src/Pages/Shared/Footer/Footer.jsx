ae3bfe5590418c10bb0c4338c02c4304139aef31
Fahime <fahim.mohammad.siam@gmail.com>
2023-12-21 00:23:35 +0600

    Completed Footer Part

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,6 +1,14 @@
 // @ts-nocheck
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer footer-center p-4 bg-[#392467] text-base-content">
+      <aside>
+        <p className="text-white">
+          Copyright © 2023 - All right reserved by SCC Technovision Inc
+        </p>
+      </aside>
+    </footer>
+  );
 };
 
 export default Footer;
