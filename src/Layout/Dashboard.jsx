bdb9d02abbecc567f6dc0e75d1a74c7cd3314a94
Fahime <fahim.mohammad.siam@gmail.com>
2023-12-22 15:42:08 +0600

    Added clickable links to the footer

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -2,9 +2,22 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { MdManageHistory } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
 import SectionTitle from "../Components/SectionTitle/SectionTitle";
+import useAuth from "../Hooks/useAuth";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+  const { logOut } = useAuth();
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        toast.success("Successfully Logged Out");
+      })
+      .catch(() => {
+        toast.error("Logout Action Failed");
+      });
+  };
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-[#5D3587]">
@@ -21,6 +34,12 @@ const Dashboard = () => {
               Home
             </NavLink>
           </li>
+          <div className="divider mt-10"></div>
+          <button onClick={handleLogOut} className="btn btn-ghost text-white">
+            <div className="flex gap-2">
+              <IoIosLogOut className="-ml-28" /> Sign Out
+            </div>
+          </button>
         </ul>
       </div>
       <div className="flex-1">
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 // @ts-nocheck
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="footer footer-center p-4 bg-[#392467] text-base-content">
@@ -6,6 +9,29 @@ const Footer = () => {
         <p className="text-white">
           Copyright © 2023 - All right reserved by SCC Technovision Inc
         </p>
+        <div className="flex text-2xl space-x-5 text-white">
+          <a
+            href="https://www.facebook.com/Fahim.Mohammad.Siam/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaFacebookSquare />
+          </a>
+          <a
+            href="https://github.com/Fahim-M-Siam"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/fahim-mohammad-siam47/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
       </aside>
     </footer>
   );
